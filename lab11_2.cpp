dd35d243e817d4ac72c290dbfe9d19e2a97a45e9
BoegyNat <bboegy@gmail.com>
2021-12-17 16:22:41 +0700

    Finished lap11

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,9 +9,15 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	dest << "-------------------- BOOM ---------------------\n";
+	string text;
+	while (getline(source,text)){
+		dest << text << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
+	
+	
+	
     source.close();
     dest.close();
 	return 0;
